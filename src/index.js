8fe8614ac08ee1f8dea90d9c94e40866d1c3ed80
ashleycarmel <itsashleycarmel@gmail.com>
2024-01-05 16:17:43 -0500

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 function displayPoem(response) {
-    new Typewriter("#poem", {
-      strings: response.data.answer,
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 function generatePoem(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let instructionsInput = document.querySelector("#user-instructions");
-let apiKey = "2tb309aa3ed47f5abf7beafo0af0d0b1";
-let prompt = `User instructions: Generate a beautiful poem about ${instructionsInput.value}`;
-let context = "You are an expert poet who loves to write beautiful poetry. Your mission is to generate a 4 line poem in basic HTML. Do not include a title to the poem. At the end of each poem write with 'With Love, AI' inside a <strong><em> element. Please follow user instructions.";
-let apiUrl =
-  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2tb309aa3ed47f5abf7beafo0af0d0b1";
+  let prompt = `User instructions: Generate a beautiful poem about ${instructionsInput.value}`;
+  let context =
+    "You are an expert poet who loves to write beautiful poetry. Your mission is to generate a 4 line poem in basic HTML. Do not include a title to the poem. At the end of each poem write with 'With Love, AI' inside a <strong><em> element. Please follow user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a beautiful poem about ${instructionsInput.value}...⏳`;
 
   axios.get(apiUrl).then(displayPoem);
 }
